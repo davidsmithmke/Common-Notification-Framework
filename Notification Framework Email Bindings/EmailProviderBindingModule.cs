06bff38364eedcbae35b4ba47c178b2d2f389e1d
David Smith <dsmith@davidsmithmke.com>
2020-01-04 18:41:22 -0600

    Add email functionality Ninject bindings

---
+++ Notification Framework Email Bindings/EmailProviderBindingModule.cs
@@ -0,0 +1,15 @@
+﻿using MountMaryUniversity.Crosscutting.Notifications.Core.Providers;
+using MountMaryUniversity.Crosscutting.Notifications.Email;
+using Ninject.Modules;
+
+namespace MountMaryUniversity.Crosscutting.Notifications.Bindings.Email
+{
+    public class EmailProviderBindingModule
+        : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<INotificationProvider>().To<EmailProvider>().WhenTargetHas<EmailNotificationAttribute>().InSingletonScope();
+        }
+    }
+}
