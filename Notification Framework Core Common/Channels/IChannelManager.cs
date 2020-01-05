a0dbe9866a1425af06052b25632adb26db944d92
David Smith <dsmith@davidsmithmke.com>
2020-01-04 18:17:09 -0600

    Add channel functionality

---
+++ Notification Framework Core Common/Channels/IChannelManager.cs
@@ -0,0 +1,12 @@
+﻿using MountMaryUniversity.Crosscutting.Notifications.Core.Providers;
+
+namespace MountMaryUniversity.Crosscutting.Notifications.Core.Channels
+{
+    public interface IChannelManager
+    {
+        INotificationChannel CreateChannel<T>(INotificationChannelConfiguration configuration) where T : INotificationChannel, new();
+        INotificationChannel GetChannel(string name);
+        IChannelManager RegisterChannel(INotificationChannel channel);
+        INotificationChannel RegisterChannel<T>(INotificationChannelConfiguration configuration) where T : INotificationChannel, new();
+    }
+}
