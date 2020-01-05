33b3bc5152b9c96cf1011b51f727874f3a454454
David Smith <dsmith@davidsmithmke.com>
2020-01-05 14:37:35 -0600

    Add provider manager

---
+++ Notification Framework Core Common/Providers/IProviderManager.cs
@@ -0,0 +1,10 @@
+﻿namespace MountMaryUniversity.Crosscutting.Notifications.Core.Providers
+{
+    public interface IProviderManager
+    {
+        INotificationProvider CreateProvider(INotificationProviderConfiguration configuration);
+        INotificationProvider GetProvider(string name);
+        INotificationProvider RegisterProvider(INotificationProvider provider);
+        INotificationProvider RegisterProvider(INotificationProviderConfiguration configuration);
+    }
+}
---
+++ Notification Framework Core/Console/ConsoleProviderConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using MountMaryUniversity.Crosscutting.Notifications.Core.Providers;
+using System;
+
+namespace MountMaryUniversity.Crosscutting.Notifications.Core.Console
+{
+    public class ConsoleProviderConfiguration
+        : INotificationProviderConfiguration
+    {
+        public string Name { get; set; }
+        public Type ProviderType => typeof(ConsoleProvider);
+    }
+}
