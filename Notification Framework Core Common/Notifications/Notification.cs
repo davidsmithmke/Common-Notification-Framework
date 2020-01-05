8bcc08f9979b6cb1e1f07c370aebd1d341128d55
David Smith <dsmith@davidsmithmke.com>
2020-01-04 18:13:23 -0600

    Add notification functionality

---
+++ Notification Framework Core Common/Notifications/Notification.cs
@@ -0,0 +1,10 @@
+﻿namespace MountMaryUniversity.Crosscutting.Notifications.Core.Notifications
+{
+    public class Notification
+        : NotificationBase, INotification
+    {
+        public Notification()
+            : base() { }
+
+    }
+}
