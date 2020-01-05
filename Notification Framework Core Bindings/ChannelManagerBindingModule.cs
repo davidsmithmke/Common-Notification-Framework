004becfd5c90de39b71eb357b932ac9e838dfe33
David Smith <dsmith@davidsmithmke.com>
2020-01-04 18:18:39 -0600

    Add core functionality Ninject bindings

---
+++ Notification Framework Core Bindings/ChannelManagerBindingModule.cs
@@ -0,0 +1,14 @@
+﻿using MountMaryUniversity.Crosscutting.Notifications.Core.Channels;
+using Ninject.Modules;
+
+namespace MountMaryUniversity.Crosscutting.Notifications.Bindings.Core
+{
+    public class ChannelManagerBindingModule
+        : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IChannelManager>().To<ChannelManager>().InSingletonScope();
+        }
+    }
+}
---
+++ Notification Framework Core Bindings/TemplateBindingModule.cs
@@ -0,0 +1,14 @@
+﻿using MountMaryUniversity.Crosscutting.Notifications.Core.Templates;
+using Ninject.Modules;
+
+namespace MountMaryUniversity.Crosscutting.Notifications.Bindings.Core
+{
+    public class TemplateBindingModule
+        : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<ITemplateManager>().To<TemplateManager>().InSingletonScope();
+        }
+    }
+}
